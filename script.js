3a5b8486bf53203c5632b6b5a2815fa960954546
nd1868640-glitch <nd1868640@gmail.com>
2026-01-12 23:52:09 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("scroll-button");
+const circle = btn.querySelector("circle");
+const totalLength = 126;
+
+window.addEventListener("scroll", (e) => {
+
+
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  const scrolled = 100 - (scrollTop / (scrollHeight - clientHeight) * 100);
+
+  circle.style.strokeDashoffset = scrolled;
+
+  btn.style.display = scrollTop > 150 ? "flex" : "none";
+});
+
+btn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behaviour: "smooth" });
+});
